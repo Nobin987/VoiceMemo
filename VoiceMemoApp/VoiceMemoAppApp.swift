6561ccc8522b08dced2cf84e1c764ca2ac8a7381
Nobin987 <nobinnepolian987@gmail.com>
2025-09-02 11:06:27 +0400

    Initial Commit

---
+++ VoiceMemoApp/VoiceMemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceMemoAppApp.swift
+//  VoiceMemoApp
+//
+//  Created by Nobin Nepolian on 02/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct VoiceMemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
